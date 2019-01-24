b29fcbd019377d837eb1f22c90f338d74b30225e
Naief Jobsen <gusjobna@student.gu.se>
2019-01-24 23:17:15 +0100

    [DOC.] Added exercise 3's folder. Code is yet to be implemented

---
+++ WP1/Exercise1-3-c/exerc13c.c
@@ -0,0 +1,11 @@
+/*°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°
+File name: exerc_1_3.c
+Date: 25-26 Jan, 2019
+Group number: 4
+Memebers that contributed to the solution:
+- Naief Jobsen
+- Filip Fatic
+- David Lindgren
+Memebers not present at demo time:
+none.
+*/
