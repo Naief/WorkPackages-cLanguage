f732d00d561ad4971629176a10a693f18333f782
Naief Jobsen <gusjobna@student.gu.se>
2019-02-07 10:38:13 +0100

    exercise 2.8

---
+++ WP2/Exercise2-8-c/exerc2_8c.c
@@ -0,0 +1,82 @@
+/*
+Nim Game
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+
+#define MAX_COINS 13
+const int HUMAN = 0;
+const int COMPUTER = 1;
+const int buffsize = 100;
+char input[buffsize];
+int result = 0;
+
+int main()
+{
+    int pile, /* This is how many coins we have */ 
+    player, /* Who is playing? */ 
+    n_coins; /* Number of coins taken */
+    srand(time(0));/* Setup random */
+    printf("Välkommen till NIM by ...");
+    pile = MAX_COINS; /* Set start values (= init) */
+    player = HUMAN;   /**  Program main loop*/
+    while (true)
+    {
+        printf("Det ligger %d  mynt i högen\n", pile);
+        if (player == HUMAN)
+        {
+            n_coins = human_choice(pile);
+        }
+        else
+        {
+            n_coins = computer_choice(pile);
+            printf("- Jag tog %d\n", n_coins);
+        }
+        pile -= n_coins;
+        player = toggle(player);
+        if (pile <= 1)
+        {
+            break;
+        }
+    } /**  end main loop*/
+    write_winner(player);
+    printf("Avslutat\n");
+    return 0;
+} 
+
+/********************************************************  DEFINITIONS*******************************************************/
+   
+    void clear_stdin()
+    {
+        while (getchar() != '\n')
+        {
+            ;
+        }
+    }
+    
+    int human_choice(int pile) {
+        while(input){
+            fgets(input, buffsize, stdin);
+            if (input >= 1 && input <=3 && input < pile){
+                input = result;
+            }
+        }
+        return result;
+    }
+
+    int computer_choice(int pile) {
+        
+
+    }
+    void write_winner(int player) {
+
+    }
+    int play_again() {
+
+    }
+    int toggle(int player) {
+        
+    }
